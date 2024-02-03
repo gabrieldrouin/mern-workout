bec0010a432b94bbe87eb49d036d125de5937caa
gabrieldrouin <gabdrouin8@gmail.com>
2024-02-03 16:50:41 -0500

    Complete first React UI

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mern-workout
 Credit to https://netninja.dev/ for the course content
+
+Simple CRUD app for managing workout exercices
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react"
+
+const Home = () => {
+
+    useEffect(() => {
+        
+    }, [])
+
+    return (
+        <div className="home">
+            <h2>Accueil</h2>
+        </div>
+    )
+}
+
+export default Home
