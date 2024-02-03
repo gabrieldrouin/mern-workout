a6692f926a8d82401885876dcd032422c1ac746d
gabrieldrouin <gabdrouin8@gmail.com>
2024-02-03 10:10:16 -0500

    Setup Mongo Model & Schemas

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -13,9 +14,16 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-    req.body
-    res.json({mssg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+    const {title, sets, reps, rest} = req.body
+
+    try {
+        const workout = await Workout.create({title, sets, reps, rest})
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+
+    }
 })
 
 // DELETE a workout
